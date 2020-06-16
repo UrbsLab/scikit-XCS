58c39fbadfc1321365e8487bf5f1b276c08fbad8
robertfrankzhang <robertzhang100@gmail.com>
2020-06-16 15:20:29 -0400

    string enum fast

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md","r") as fh:
 setup(
   name = 'scikit-XCS',
   packages = ['skXCS'],
-  version = '1.0.4',
+  version = '1.0.5',
   license='License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
   description = 'XCS Learning Classifier System',
   long_description_content_type="text/markdown",
   author = 'Robert Zhang, Ryan J. Urbanowicz',
   author_email = 'robertzh@seas.upenn.edu,ryanurb@upenn.edu',
   url = 'https://github.com/UrbsLab/scikit-XCS',
-  download_url = 'https://github.com/UrbsLab/scikit-XCS/archive/v_1.0.4.tar.gz',
+  download_url = 'https://github.com/UrbsLab/scikit-XCS/archive/v_1.0.5.tar.gz',
   keywords = ['machine learning','data analysis','data science','learning classifier systems','xcs'],
   install_requires=['numpy','pandas','scikit-learn'],
   classifiers=[
